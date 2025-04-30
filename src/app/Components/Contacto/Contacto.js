5b4eddf819c44e33af0ac3adbf277b2c13ec692f
RomanV <romanvalles01@gmail.com>
2025-04-30 06:07:19 -0300

    update

--- src/app/Components/Contacto/Contacto.js
+++ src/app/Components/Contacto/Contacto.js
@@ -1,51 +1,56 @@
 "use client";
 import React from "react";
 import styles from "./contacto.module.css";
+import Image from "next/image";
+import pfp from "../../statics/pfp.png";
+import mailSvg from "../../statics/mailSvg.svg";
 
 export default function Contacto() {
   return (
     <section id="contacto" className={styles.contactoSection}>
-      <div className={styles.formContainer}>
-        <span className={styles.leftBarBack}></span>
-        <span className={styles.leftBar}></span>
-        <form
-          className={styles.form}
-          action="mailto:tuemail@ejemplo.com"
-          method="POST"
-          encType="text/plain"
-        >
-          <h2 className={styles.title}>Contacto</h2>
-
-          <label className={styles.label}>
-            Nombre
-            <input type="text" name="name" required className={styles.input} />
-          </label>
-
-          <label className={styles.label}>
-            Email
-            <input
-              type="email"
-              name="email"
-              required
-              className={styles.input}
-            />
-          </label>
-
-          <label className={styles.label}>
-            Mensaje
-            <textarea
-              name="message"
-              rows="4"
-              required
-              className={styles.textarea}
-            />
-          </label>
-
-          <button type="submit" className={styles.button}>
-            Enviar mensaje
-          </button>
-        </form>
-      </div>
+      <article className={styles.messageContainer}>
+        <div className={styles.superImageContainer}>
+          <span className={styles.topBarContainer}>
+            <span className={styles.topBar}></span>
+            <span className={styles.topBarBack}></span>
+          </span>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.pfp}
+              src={pfp}
+              width={167}
+              height={224}
+              alt="Roman Valles"
+            ></Image>
+          </div>
+        </div>
+        <div className={styles.contentContainer}>
+          <div>
+            <span className={styles.newMessageContainer}>
+              <Image src={mailSvg} height={45} width={45} alt="Mail"></Image>
+              <h1 className={styles.newMessage}>NEW MESSAGE</h1>
+            </span>
+            <h2 className={styles.name}>ROMAN VALLES</h2>
+          </div>
+          <div>
+            <span className={styles.responseContainerBack}></span>
+            <div className={styles.responseContainer}>
+              <p className={styles.responseText}>
+                ¿Tienes algún proyecto en mente? Estoy abierto a propuestas
+                laborales o colaboraciones freelance que combinen desarrollo y
+                visión creativa.
+              </p>
+              <span className={styles.footerText}>
+                <span className={styles.footerContact}>
+                  <p>Contactame:</p>
+                  <p>romanvalles01@gmail.com</p>
+                </span>
+                <p>linkedin</p>
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
     </section>
   );
 }
