ccd5cf03ad73cdd6985cf23cbbade7fde0ea0c64
RomanV <romanvalles01@gmail.com>
2025-06-04 06:12:02 -0300

    add meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@
 import "./globals.css";
 import Header from "./Components/Header/Header";
 import { Rajdhani, Orbitron } from "next/font/google";
-
+import Head from "next/head"; // 👈 Agregá esto
 import { ParallaxProvider } from "react-scroll-parallax";
 
 const rajdhaniFont = Rajdhani({
@@ -20,6 +20,45 @@ const orbitronFont = Orbitron({
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <Head>
+        <title>Román Valles</title>
+        <meta
+          name="description"
+          content="Portfolio Román Valles, Frontend Developer y Diseñador UI/UX."
+        />
+
+        {/* Open Graph / Facebook / LinkedIn */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.romanvalles.dev" />
+        <meta
+          property="og:title"
+          content="Román Valles – Frontend Dev / UI UX"
+        />
+        <meta
+          property="og:description"
+          content="Portfolio Román Valles, Frontend Developer y Diseñador UI/UX."
+        />
+        <meta
+          property="og:image"
+          content="https://www.romanvalles.dev/statics/cardHead.png"
+        />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="https://www.romanvalles.dev" />
+        <meta
+          name="twitter:title"
+          content="Román Valles – Frontend Dev / UI UX"
+        />
+        <meta
+          name="twitter:description"
+          content="Portfolio Román Valles, Frontend Developer y Diseñador UI/UX."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.romanvalles.dev/statics/cardHead.png"
+        />
+      </Head>
       <body className={`${rajdhaniFont.className} ${orbitronFont.className}`}>
         {" "}
         <Header></Header>
